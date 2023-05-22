20e6d003fec21990c9f64a0cbde6c5ce0b00b6fe
Cunfuu <cunfuu@hotmail.com>
2023-05-23 00:41:51 +0300

    refactor?

--- users.tf
+++ users.tf
@@ -6,6 +6,7 @@ resource "aws_iam_user" "admin_user" {
   name = "admin-user"  # Replace with the desired username
 }
 
+
 resource "aws_iam_user_policy_attachment" "admin_user_attachment" {
   user       = aws_iam_user.admin_user.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
